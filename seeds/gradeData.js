2a497aa13faeae7a5309fccbfbf7e987ba01f0e5
raied Abdelgani <raiedabdelgani@gmail.com>
2022-12-20 21:43:47 -0500

    added class subjects and grade letters

--- seeds/gradeData.js
+++ seeds/gradeData.js
@@ -1,9 +1,55 @@
 const { Grade } = require('../models');
 
 const gradedata = [
-
-
+{
+    letter_grade: 'A+',
     
+  },
+  {
+    letter_grade: 'A',
+    
+  },
+  {
+    letter_grade: 'A-',
+    
+  },
+  {
+    letter_grade: 'B+',
+   
+  },
+  {
+    letter_grade: 'B',
+    
+  },
+  {
+    letter_grade: 'B-',
+    
+  },
+  {
+    letter_grade: 'C+',
+  },
+  {
+    letter_grade: 'C',
+    
+  },
+  {
+    letter_grade: 'C-',
+    
+  },
+  {
+    letter_grade: 'D+',
+  },
+  {
+    letter_grade: 'D',
+  },
+  {
+    letter_grade: 'D-',
+  },
+  {
+    letter_grade: 'F',
+  },
+
+
 ];
 
 const seedGrade = () => Grade.bulkCreate(gradedata);
