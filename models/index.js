d964b4f9ad79e4d6de4101af863e9d761231e257
Cody Kirschbaum <baumerc10@gmail.com>
2023-01-03 18:53:45 -0500

    Updated index to reflect new user model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,17 @@
+const User = require('./User');
 const Class = require('./Class');
 const Student = require('./Student');
 
+Student.hasMany(Class, {
+    foreignKey: 'student_id',
+});
+
+Class.belongsTo(Student, {
+    foreignKey: 'student_id',
+});
+
 module.exports = {
+    User,
     Class, 
     Student
 }
